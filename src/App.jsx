1f61e169d8e5b9843380fe6d24c92fc06c8d2b33
taliyameyswara <taliyameyswara@gmail.com>
2023-08-25 09:56:55 +0700

    jadwal blm selese

--- src/App.jsx
+++ src/App.jsx
@@ -42,26 +42,41 @@ function App() {
 						<i className="nes-icon is-medium star"></i>
 					</div>
 				</div>
-				<div>
-					<h1>SKILLS</h1>
-					<h2>mendapatkan soft skill dan hard skil</h2>
-				</div>
-				<div>
-					<h1>PENGALAMAN</h1>
-					<h2>mendapatkan soft skill dan hard skil</h2>
-				</div>
-				<div>
-					<h1>RELASI</h1>
-					<h2>mendapatkan soft skill dan hard skil</h2>
-				</div>
-				<div>
-					<h1>SERTIFIKAT</h1>
-					<h2>mendapatkan soft skill dan hard skil</h2>
+				<div className="benefit-content">
+					<div className="benefit-item">
+						<h1 className="benefit-title">
+							<i className="nes-icon is-medium like"></i>
+							<span>skill</span>
+						</h1>
+						<h2>Mendapatkan softskill dan hardskill</h2>
+					</div>
+					<div className="benefit-item">
+						<h1 className="benefit-title">
+							<i className="nes-icon is-medium like"></i>
+							<span>pengalaman</span>
+						</h1>
+						<h2>Mendapatkan pengalaman belajar bersama</h2>
+					</div>
+					<div className="benefit-item">
+						<h1 className="benefit-title">
+							<i className="nes-icon is-medium like"></i>
+							<span>relasi</span>
+						</h1>
+						<h2>Menambah relasi antar peserta dan mentor</h2>
+					</div>
+					<div className="benefit-item">
+						<h1 className="benefit-title">
+							<i className="nes-icon is-medium like"></i>
+							<span>sertifikat</span>
+						</h1>
+						<h2>Mendapatkan sertifikat hasil pelatihan</h2>
+					</div>
 				</div>
 			</section>
 
 			<section className="jadwal">
-				<h1>JADWAL</h1>
+				<h1 className="title">jadwal</h1>
+
 				<ul>
 					<li>
 						<p>Pendaftaran : Jumat, 8 September - Jumat, 22 September</p>
@@ -76,7 +91,7 @@ function App() {
 						<p>Penutupan : Sabtu, 28 Oktober 2023</p>
 					</li>
 				</ul>
-				<h1>BTNG 2023</h1>
+				<h1 className="title">btng 2023</h1>
 			</section>
 			<section className="devisi">
 				<div className="devisi-mobil">
