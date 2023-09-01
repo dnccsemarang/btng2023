96a0b280bd0a724903f481c3a4583eca5d398d8f
taliyameyswara <taliyameyswara@gmail.com>
2023-09-01 22:23:32 +0700

    👌fix: responsive

--- src/components/jadwal.jsx
+++ src/components/jadwal.jsx
@@ -47,14 +47,9 @@ const Jadwal = () => {
 	);
 };
 
-const JadwalGambar = styled.div`
-	/* float: left; */
-`;
+const JadwalGambar = styled.div``;
 
-const Gambar1 = styled.div`
-	/* margin-left: 0 !important;
-	margin-right: 0 !important; */
-`;
+const Gambar1 = styled.div``;
 
 const JamurIMG = styled.img`
 	@media screen and (min-width: 320px) {
@@ -79,7 +74,6 @@ const TanahImg = styled.img`
 `;
 
 const Title = styled.span`
-	/* background-color: red; */
 	color: #fff;
 	text-align: center;
 	text-shadow: 0px 4px 0px #000;
@@ -101,41 +95,9 @@ const Title = styled.span`
 		text-shadow: 0px 2px 0px #000;
 		filter: drop-shadow(2px 2px #000);
 	}
-	/* @media screen and (min-width: 320px) { */
-	/* color: #fff;
-		text-align: center;
-		text-shadow: 0px 4px 0px #000;
-		filter: drop-shadow(2px 2px #000);
-		-webkit-text-stroke-width: 1px;
-		-webkit-text-stroke-color: black;
-		font-family: ArcadeClassic;
-		font-size: 40px;
-		font-style: normal;
-		font-weight: 400;
-		line-height: normal; */
-	/* margin-bottom: 40px; */
-	/* } */
-	/* @media screen and (min-width: 600px) {
-		color: #fff;
-		text-align: center;
-		text-shadow: 0px 4px 0px #000;
-		filter: drop-shadow(2px 2px #000);
-		-webkit-text-stroke-width: 1px;
-		-webkit-text-stroke-color: black;
-		font-family: ArcadeClassic;
-		font-size: 88px;
-		font-style: normal;
-		font-weight: 400;
-		line-height: normal;
-		/* margin-bottom: 40px; */
-	/* } */
 `;
 
 const Pixel = styled.ul`
-	/* flex-direction: row; */
-	/* align-items: center; */
-	/* padding: 30px; */
-	/* background-color: red; */
 	padding: 1vw;
 	padding-inline: 2vw;
 `;
@@ -166,21 +128,17 @@ const TanahIMG = styled.img`
 
 const Gambar2 = styled.div`
 	display: grid;
-	justify-items: end; /* Atau 'right' */
+	justify-items: end;
 `;
 
 const Text = styled.div`
 	padding: 10px;
 	font-size: 4vw;
 	font-family: VT323;
-	/* margin: 20px; */
-	/* background-color: #0074da; */
 	@media screen and (min-width: 320px) {
-		/* font-size: 14px; */
 		margin: 10px;
 	}
 	@media screen and (min-width: 600px) {
-		/* font-size: 32px; */
 		font-size: 3vw;
 		margin: 20px;
 	}
@@ -188,13 +146,6 @@ const Text = styled.div`
 
 const Jadwalwrap = styled.div`
 	position: relative;
-	/* margin: auto; */
-	/* max-width: 1200px; */
-	/* margin-inline: 10vw; */
 	margin-inline: 6vw;
-
-	/* @media screen and (max-width: 600px) {
-		margin-inline: 6vw;
-	} */
 `;
 export default Jadwal;
