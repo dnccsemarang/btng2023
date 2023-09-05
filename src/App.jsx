d30328e4128fe36045ca049d84d58cad75233acd
taliyameyswara <taliyameyswara@gmail.com>
2023-09-05 23:58:58 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ import Benefit from './components/Body/Benefit/Benefit';
 import Footer from './components/Footer/Footer';
 import Jadwal from './components/Body/Jadwal/Jadwal';
 
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+// import Aos from 'aos';
+// import 'aos/dist/aos.css';
+// import { useEffect } from 'react';
 
 function App() {
-	useEffect(() => {
-		Aos.init({ duration: 600 });
-	}, []);
+	// useEffect(() => {
+	// 	Aos.init({ duration: 600 });
+	// }, []);
 
 	return (
 		<div className="container">
