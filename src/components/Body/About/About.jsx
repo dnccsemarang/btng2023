2c35c99cfcea8ed87f16c2e439c0a2d318550636
taliyameyswara <taliyameyswara@gmail.com>
2023-09-07 21:55:42 +0700

    remove typing effect

--- src/components/Body/About/About.jsx
+++ src/components/Body/About/About.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { useTypewriter } from 'react-simple-typewriter';
+// import { useTypewriter } from 'react-simple-typewriter';
 
 function About() {
-	const [typeEffect] = useTypewriter({
-		words: [
-			'BTNG (Basic Training Next Generation) adalah acara DNCC sebagai gerbang awal bagi mahasiswa Universitas Dian Nuswantoro semester 1 sampai semester 5 yang ingin menjadi anggota di UKM DNCC. Acara ini bertujuan untuk memberikan pelatihan kepada calon anggota baru agar memiliki pengetahuan yang unggul dalam divisi yang dipilih.',
-		],
-		loop: 1,
-		typeSpeed: 30,
-	});
+	// const [typeEffect] = useTypewriter({
+	// 	words: [
+	// 		'BTNG (Basic Training Next Generation) adalah acara DNCC sebagai gerbang awal bagi mahasiswa Universitas Dian Nuswantoro semester 1 sampai semester 5 yang ingin menjadi anggota di UKM DNCC. Acara ini bertujuan untuk memberikan pelatihan kepada calon anggota baru agar memiliki pengetahuan yang unggul dalam divisi yang dipilih.',
+	// 	],
+	// 	loop: 1,
+	// 	typeSpeed: 30,
+	// });
 	return (
 		<section className="about" id="about">
 			<h1 className="title with-arrows" data-aos="zoom-in" data-aos-offset="250" data-aos-duration="320">
 				About BTNG
 			</h1>
 			<div className="nes-balloon balloon" data-aos="zoom-in-down">
-				<p style={{ textAlign: 'justify' }}>{typeEffect}</p>
+				<p style={{ textAlign: 'justify' }}>
+					{' '}
+					BTNG (Basic Training Next Generation) adalah acara DNCC sebagai gerbang awal bagi mahasiswa Universitas Dian
+					Nuswantoro semester 1 sampai semester 5 yang ingin menjadi anggota di UKM DNCC. Acara ini bertujuan untuk
+					memberikan pelatihan kepada calon anggota baru agar memiliki pengetahuan yang unggul dalam divisi yang
+					dipilih.
+				</p>
 				<div className="bubble-buttom"></div>
 			</div>
 			<div className="item-about">
